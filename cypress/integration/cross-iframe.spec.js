8b47c46286175dbb89754012608bf7cd380eab4a
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-03-07 18:22:12 +0300

    Delete cypress directory

--- cypress/integration/cross-iframe.spec.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Cross domain iframes', () => {
-  // this test will fail unless you set chromeWebSecurity to false
-  it('should let you switch to the iframe', () => {
-    cy.visit('index.html');
-    cy.switchToIframe('[data-cy="test-iframe"]');
-  });
-});
--- cypress/support/commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('switchToIframe', (iframe) => {
-  return cy
-    .get(iframe)
-    .its('0.contentDocument.body')
-    .should('be.visible')
-    .then(cy.wrap);
-});
