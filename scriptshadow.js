8520e0cc72bd9151d28122c56150076d9668e11a
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-03-28 01:56:23 +0300

    Update scriptshadow.js

--- scriptshadow.js
+++ scriptshadow.js
@@ -3,7 +3,7 @@ var root = containerrr.attachShadow( { mode: "open" } )
 
 //Inside element
 var h1 = document.createElement( "h1" )
-h1.textContent = "Inside Shadow DOM"
+h1.textContent = "Insideeeee Shadow DOM"
 h1.id = "inside"
 root.appendChild( h1 )
 
