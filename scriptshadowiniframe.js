7cf0c7749af2d96923c7439081848a166376d970
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-03-28 03:38:28 +0300

    Update scriptshadowiniframe.js

--- scriptshadowiniframe.js
+++ scriptshadowiniframe.js
@@ -1,11 +1,13 @@
 (function (){
 var root = shadowwcontainer.attachShadow( { mode: "open" } )
 
+/*
 //Inside element
 var h1 = document.createElement( "h1" )
 h1.textContent = "Inside Shadow DOM"
 h1.id = "inside"
 root.appendChild( h1 )
+*/
 
   //example 2 button inside shadow
 var buttonnn = document.createElement( "button" )
