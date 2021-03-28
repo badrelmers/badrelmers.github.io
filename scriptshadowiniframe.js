fd4020008bdde5f9ac8fdce648a29aa72bd68f9a
Badr Elmers <40046489+badrelmers@users.noreply.github.com>
2021-03-28 03:48:05 +0300

    Update scriptshadowiniframe.js

--- scriptshadowiniframe.js
+++ scriptshadowiniframe.js
@@ -15,6 +15,11 @@ buttonnn.textContent = "buttonnn Inside Shadow DOM"
 buttonnn.id = "Butonnninside"
 root.appendChild( buttonnn )
   
+  //example 2 button inside shadow
+var buttonnn2 = document.createElement( "button" )
+buttonnn2.textContent = "buttonnn2222 Inside Shadow DOM"
+buttonnn2.id = "Butonnn22inside"
+root.appendChild( buttonnn2 )
   
 //Access inside element
 console.log( shadowwcontainer.shadowRoot.querySelector( "#inside" ) )
